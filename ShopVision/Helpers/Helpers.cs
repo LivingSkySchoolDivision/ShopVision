a20fb5bc2ad285931a6152af95de470385db02d5
Mark Strendin <mark.strendin@lskysd.ca>
2023-10-16 14:19:20 -0600

    Don't allow \ characters in vehicle names Before converting special characters, so we can allow escaped quotes, just not the naked "\" character.

--- ShopVision/Helpers/Helpers.cs
+++ ShopVision/Helpers/Helpers.cs
@@ -192,9 +192,9 @@ namespace ShopVision
         {
             List<char> allowedCharacters = new List<char>()
             {
-                'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9','0','-','=','_','+','\'','"','/','?','!','@','#','$','%','^','&','*','(',')','.',',',';',':','\\','<','>',' ','\n'
+                'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9','0','-','=','_','+','\'','"','/','?','!','@','#','$','%','^','&','*','(',')','.',',',';',':','<','>',' ','\n'
             };
-
+            
             StringBuilder returnMe = new StringBuilder();
             
             foreach (char c in inputString)
@@ -205,7 +205,7 @@ namespace ShopVision
                 }
             }
 
-            return returnMe.ToString().Replace("\n","<br>").Replace("\"", "\\\"").Replace("<div></div>", string.Empty).Replace("/", "\\/");
+            return returnMe.ToString().Replace("\n", "<br>").Replace("\"", "\\\"").Replace("<div></div>", string.Empty).Replace("/", "\\/");
         }
 
         public static string GetMonthName(int monthNum)
